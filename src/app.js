1cbc810822f39259469f8dcf42118f7f8e490c37
savannah20 <45461035+savannah20@users.noreply.github.com>
2024-03-14 07:44:05 -0500

    Inject the forecast HTML from Javascript

--- src/app.js
+++ src/app.js
@@ -74,7 +74,36 @@ function search(event) {
   let city = cityInput.value;
   searchCity(city);
 }
+
+function displayForecast() {
+  let forecast = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      ` 
+         <div class="forecast-day">
+              <div class="forecast-date">${day}</div>
+              <img
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                alt=""
+                width="42"
+              />
+              <div class="forecast-temperature">
+                <span class="forecast-temperature-max">18°</span>
+                <span class="forecast-temperature-min">12°</span>
+              </div>
+            </div>`;
+  });
+  forecast.innerHTML = forecastHtml;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
 searchCity("Kingston");
+displayForecast();
